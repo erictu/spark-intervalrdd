19a51100b3a837a14a665fcb2e1584774f4fee53
Eric Tu <erictctu@gmail.com>
2015-09-30 19:54:59 -0700

    try 1

--- src/main/scala/edu/berkeley/cs/amplab/spark/intervalrdd/IntervalRDD.scala
+++ src/main/scala/edu/berkeley/cs/amplab/spark/intervalrdd/IntervalRDD.scala
@@ -37,31 +37,54 @@ class IntervalRDD[K: ClassTag, V: ClassTag](
   extends RDD[(K, V)](partitionsRDD.context, List(new OneToOneDependency(partitionsRDD))) {
   
   // add bookkeeping private structure
+  // String is the chromosome/location, Long is partition number
+  // TODO: make this more generalizable
+  var bookkeep: IntervalTree[String, Long] = new IntervalTree[String, Long]()
 
-  /** Gets the value corresponding to the specified key, if any. */
-  def get(k: K): Option[V] = multiget(Array(k)).get(k)
+  /** Gets the value corresponding to the specified request, if any. 
+  * a request contains the chromosome, interval, and specified keys
+  */
+  def get(chr: String, intl: Interval[Long], k: K): Option[V] = multiget(chr, intl, Array(k))
 
-  /** Gets the values corresponding to the specified key, if any */
-  def multiget(ks: Array[K]): Map[K, V] = {
-    // check bookkeeping structure for partition
-    // fetch data from rdd
+  // Gets values corresponding to a single interval over multiple ids
+  def get(chr: String, intl: Interval[Long]): Option[V] = multiget(chr, intl, None)
+
+  /** Gets the values corresponding to the specified key, if any 
+  * Assume that we're only getting data that exists (if it doesn't exist,
+  * would have been handled by upper LazyMaterialization layer
+  */
+  def multiget(chr: String, intl: Interval[Long], ks: Option[Array[K]]): Map[K, V] = {
+    // check bookkeeping structure for partition number
+    var partitionNum = bookkeep.search(intl, chr)
+
+    var partition = partitionsRDD.getPartition(partitionNum) // TODO: Assume this exists
+    // data exists in RDD, and we have the partition. Now let's fetch it
+    var fetchedData = None
+    ks match {
+      case Some(ks)  => fetchedData = partition.get(intl, ks)
+      case None      => fetchedData = partition.get(intl)
+    }
+    fetchedData
   }
 
   /**
    * Unconditionally updates the specified key to have the specified value. Returns a new IntervalRDD
    * that reflects the modification.
    */
-  def put(k: K, v: V): IntervalRDD[K, V] = multiput(Map(k -> v)) 
+  def put(chr: String, intl: Interval[Long], k: K, v: V): IntervalRDD[K, V] = multiput(chr, intl, Map(k -> v)) 
 
   /**
    * Unconditionally updates the specified keys to have the specified value. Returns a new IntervalRDD
    * that reflects the modification.
    */
-  def multiput(kvs: Map[K, V]): IntervalRDD[K, V] = {
-    
-    // put information in partition based on chrm and interval
+  def multiput(chr: String, intl: Interval[Long], kvs: Map[K, V]): IntervalRDD[K, V] = {
+    // update bookkeeping structure to insert entries of (chr, interval)
+    // in the bookkeeping structure place the partition number
+    // TODO: how to generate the partition number: use a hash function and put the bookkeep somewhere else?
 
-    // update bookkeeping structure
+    // then create an IntervalPartition with the subset of data provided, and the partition number, and add that to our RDD
+    // by returning a newRDD
+
   }
 
 
